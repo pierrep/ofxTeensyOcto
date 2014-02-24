8d53f3f35eed35d1a4814eaaf96af325e6c5633c
Jason Walters <hello@spirobeam.com>
2014-02-24 10:07:39 -0500

    updated example
    added demo modes, and dynamic images & video launching.

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "ofxTeensyOcto.h"
-
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
@@ -21,15 +20,60 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
-    ofxTeensyOcto teensy;
-    
-    ofFbo fbo;
-    void updateFbo();
-    
-    // variables misc
+    // LED - Teensy stuff
     //-----------------------------
+    ofxTeensyOcto teensy;
+    int stripWidth;
+    int stripHeight;
+    int stripsPerPort;
+    int numPorts;
     int brightness;
     float waveSpeed;
-    int debugScroll;
+    
+    // FBO stuff
+    //-----------------------------
+    ofFbo fbo;
+    void updateFbo();
+    void drawFbo();
+    int fboRotate;
+    bool fboFlip;
+    ofTexture tex;
+    ofPixels guiPixels;
+    
+    // gui views
+    //-----------------------------
+    void drawPanels();
+    
+    // Graphic functions
+    //-----------------------------
+    void drawDemos();
+    void drawDebug();
+    void drawRainbowH();
+    void drawRainbowV();
+    void drawWaves();
+    void drawVideos();
+    void drawImages();
+    void drawTestPattern();
+    int drawModes;
+    int demoModes;
+    float counterShape;
+    int hue;
+    
+    // Test Pattern
+    //-----------------------------
+    ofTrueTypeFont font;
+    
+    // Video player
+    //-----------------------------
+    ofDirectory dirVid;
+    vector<ofVideoPlayer> vid;
+    int currentVideo;
+    bool videoOn;
+    
+    // Image player
+    //-----------------------------
+    ofDirectory dirImg;
+    vector<ofImage> img;
+    int currentImage;
 		
 };
